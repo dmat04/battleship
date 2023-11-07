0bb3357655dfcb8be1a9ae22ba42c3938d0242ff
matej <matej.djerdji@gmail.com>
2023-11-07 16:59:00 +0100

    Document the Ship class

--- src/game/Ship.ts
+++ src/game/Ship.ts
@@ -1,9 +1,20 @@
 import assertNever from '../utils/typeUtils';
 import { ShipType } from './types';
 
+/** Maximum ship size (length) */
 export const MAX_SIZE = 5;
 
+/**
+ * Class modelling a battleship game ship.
+ * Each ship has a type property and its size (length) measured in
+ * the number of game board cells it occupies.
+ * New Ship instances cannot be created, all of the existing ship types
+ * are accessible as static member singleton instances of this class.
+ *
+ * @class Ship
+ */
 class Ship {
+  // The five available ship type instances
   private static submarineInstance: Ship;
 
   private static destroyerInstance: Ship;
@@ -14,6 +25,12 @@ class Ship {
 
   private static aircraftCarrierInstance: Ship;
 
+  /**
+   * Creates an instance of Ship.
+   * @param {ShipType} type - The type of the ship
+   * @param {number} size - The size of the ship, must be 0 < size <= MAX_SIZE
+   * @memberof Ship
+   */
   private constructor(
     readonly type: ShipType,
     readonly size: number,
@@ -27,6 +44,14 @@ class Ship {
     }
   }
 
+  /**
+   * Get the ship instance for a given ship type
+   *
+   * @static
+   * @param {ShipType} type - The type of the ship
+   * @memberof Ship
+   * @returns {Ship}
+   */
   public static Get = (type: ShipType): Ship => {
     switch (type) {
       case ShipType.Submarine:
@@ -44,6 +69,12 @@ class Ship {
     }
   };
 
+  /**
+   * Get the ShipType.Submarine Ship instance
+   *
+   * @static
+   * @memberof Ship
+   */
   public static Submarine = (): Ship => {
     if (!this.submarineInstance) {
       this.submarineInstance = new Ship(ShipType.Submarine, 1);
@@ -52,6 +83,12 @@ class Ship {
     return this.submarineInstance;
   };
 
+  /**
+  * Get the ShipType.Destroyer Ship instance
+  *
+  * @static
+  * @memberof Ship
+  */
   public static Destroyer = (): Ship => {
     if (!this.destroyerInstance) {
       this.destroyerInstance = new Ship(ShipType.Destroyer, 2);
@@ -60,6 +97,12 @@ class Ship {
     return this.destroyerInstance;
   };
 
+  /**
+  * Get the ShipType.Cruiser Ship instance
+  *
+  * @static
+  * @memberof Ship
+  */
   public static Cruiser = (): Ship => {
     if (!this.cruiserInstance) {
       this.cruiserInstance = new Ship(ShipType.Cruiser, 3);
@@ -68,6 +111,12 @@ class Ship {
     return this.cruiserInstance;
   };
 
+  /**
+  * Get the ShipType.Battleship Ship instance
+  *
+  * @static
+  * @memberof Ship
+  */
   public static Battleship = (): Ship => {
     if (!this.battleshipInstance) {
       this.battleshipInstance = new Ship(ShipType.Battleship, 4);
@@ -76,6 +125,12 @@ class Ship {
     return this.battleshipInstance;
   };
 
+  /**
+  * Get the ShipType.AircraftCarrier Ship instance
+  *
+  * @static
+  * @memberof Ship
+  */
   public static AircraftCarrier = (): Ship => {
     if (!this.aircraftCarrierInstance) {
       this.aircraftCarrierInstance = new Ship(ShipType.AircraftCarrier, 5);
