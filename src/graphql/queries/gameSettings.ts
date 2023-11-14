d051c3298ef0ae9e2aed0d27e376b430b513f823
matej <matej.djerdji@gmail.com>
2023-11-14 20:00:33 +0100

    Change resolver to adapt to GameService changes

--- src/graphql/queries/gameSettings.ts
+++ src/graphql/queries/gameSettings.ts
@@ -1,6 +1,7 @@
 import { GraphQLError } from 'graphql';
 import GameService from '../../services/GameService';
 import type { ApolloContext } from '../../middleware/ApolloMiddleware';
+import EntityNotFoundError from '../../services/errors/EntityNotFoundError';
 
 interface MutationParams {
   gameId: string;
@@ -23,15 +24,18 @@ export const resolvers = {
         });
       }
 
-      const game = GameService.getGame(args.gameId);
+      try {
+        const game = GameService.getGame(args.gameId);
+        return game.getGameSettings();
+      } catch (error) {
+        if (error instanceof EntityNotFoundError) {
+          throw new GraphQLError(`Game with id '${args.gameId}' not found.`, {
+            extensions: { code: 'BAD_USER_INPUT' },
+          });
+        }
 
-      if (!game) {
-        throw new GraphQLError(`Game with id '${args.gameId}' not found.`, {
-          extensions: { code: 'BAD_USER_INPUT' },
-        });
+        throw error;
       }
-
-      return game.getGameSettings();
     },
   },
 };
