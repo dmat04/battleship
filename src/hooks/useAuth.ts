9203f1c6a7ee1a89bb52be9c5765242b9df32556
matej <matej.djerdji@gmail.com>
2023-12-31 13:32:54 +0100

    Created useAuth hook

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import {
+  useAppDispatch as useDispatch,
+  useAppSelector as useSelector,
+} from '../store/store';
+import { REGISTERED_LOGIN } from '../graphql/mutations';
+import { LoginResult } from '../types/ServerTypes';
+import { clearAuth, setAuth } from '../store/authSlice';
+import LocalStorage from '../utils/localStorageUtils';
+
+const useAuth = () => {
+  const token = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const [doLogin, { loading }] = useMutation(
+    REGISTERED_LOGIN,
+    { fetchPolicy: 'no-cache' },
+  );
+  const [error, setError] = useState<string | null>(null);
+
+  const login = async (username: string, password: string): Promise<void> => {
+    const response = await doLogin({ variables: { username, password } });
+
+    if (response.data) {
+      const newToken = response.data.registeredLogin as LoginResult;
+      LocalStorage.saveAccessToken(newToken);
+      setError(null);
+      dispatch(setAuth(newToken));
+    }
+
+    if (response.errors) {
+      setError('Login unsuccessfull');
+    }
+  };
+
+  const logout = (): void => {
+    LocalStorage.clearAccessToken();
+    dispatch(clearAuth());
+  };
+
+  return [token, login, logout, loading, error] as const;
+};
+
+export default useAuth;
