c9f7c96adad1a443890184e0eb956b22421a2801
matej <matej.djerdji@gmail.com>
2024-01-02 20:01:34 +0100

    GameGrid - remove css border, paint it into the background-image

--- src/components/GameGrid/index.tsx
+++ src/components/GameGrid/index.tsx
@@ -1,26 +1,29 @@
 import styled from 'styled-components';
 import { Theme } from '../assets/themes/themeDefault';
 
-const Grid = styled.div<{ $cols: number, $rows: number, theme: Theme }>`
+const GameGrid = styled.div<{ $cols: number, $rows: number, theme: Theme }>`
   --cols: ${(props) => props.$cols};
   --rows: ${(props) => props.$rows};
   
   display: grid;
-  border: .2em solid black;
   grid-template-columns: repeat(var(--cols), 1fr);
   grid-template-rows: repeat(var(--rows), 1fr);
   aspect-ratio: 1;
-  background-image:  
+  background-image:
     linear-gradient(black .2em, transparent .2em),
-    linear-gradient(to right, black .2em, transparent .2em);
-  background-size: calc(100% / var(--cols)) calc(100% / var(--rows));
-  background-position: left -.1em top -.1em;
+    linear-gradient(to right, black .2em, transparent .2em),
+    linear-gradient(black .4em, transparent .4em),
+    linear-gradient(to right, black .4em, transparent .4em);
+  background-size: 
+    calc(100% / var(--cols)) calc(100% / var(--rows)),
+    calc(100% / var(--cols)) calc(100% / var(--rows)),
+    100% 100%,
+    100% 100%;
+  background-position: 
+    left -.1em top -.1em,
+    left -.1em top -.1em,
+    left -.2em top -.2em,
+    left -.2em top -.2em;
 `;
-
-const GameGrid = ({ children }: React.PropsWithChildren) => (
-  <Grid $cols={10} $rows={10}>
-    {children}
-  </Grid>
-);
 
 export default GameGrid;
