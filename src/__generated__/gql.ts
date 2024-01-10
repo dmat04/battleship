466c9a7b0e3143a7b9172f1f3c911ce46c2e50a3
matej <matej.djerdji@gmail.com>
2024-01-10 16:04:56 +0100

    Added guestLogin gql mutation

--- src/__generated__/gql.ts
+++ src/__generated__/gql.ts
@@ -13,6 +13,7 @@ import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/
  * Therefore it is highly recommended to use the babel or swc plugin for production.
  */
 const documents = {
+    "\n  mutation guestLogin($username: String) {\n    guestLogin(username: $username) {\n      accessToken\n      expiresAt\n      username\n    }\n  }\n": types.GuestLoginDocument,
     "\n  mutation registeredLogin($username: String!, $password: String!) {\n    registeredLogin(username: $username, password: $password) {\n      accessToken\n      expiresAt\n      username\n    }\n  }\n": types.RegisteredLoginDocument,
     "\n  mutation createRoom {\n    createRoom {\n      roomID\n      inviteCode\n      wsAuthCode\n    }\n  }\n": types.CreateRoomDocument,
     "\n  mutation joinRoom($inviteCode: String!) {\n    joinRoom(inviteCode: $inviteCode) {\n      roomID\n      wsAuthCode\n    }\n  }\n": types.JoinRoomDocument,
@@ -33,6 +34,10 @@ const documents = {
  */
 export function gql(source: string): unknown;
 
+/**
+ * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function gql(source: "\n  mutation guestLogin($username: String) {\n    guestLogin(username: $username) {\n      accessToken\n      expiresAt\n      username\n    }\n  }\n"): (typeof documents)["\n  mutation guestLogin($username: String) {\n    guestLogin(username: $username) {\n      accessToken\n      expiresAt\n      username\n    }\n  }\n"];
 /**
  * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
--- src/graphql/mutations.ts
+++ src/graphql/mutations.ts
@@ -1,5 +1,15 @@
 import { gql } from '../__generated__/gql';
 
+export const GUEST_LOGIN = gql(`
+  mutation guestLogin($username: String) {
+    guestLogin(username: $username) {
+      accessToken
+      expiresAt
+      username
+    }
+  }
+`);
+
 export const REGISTERED_LOGIN = gql(`
   mutation registeredLogin($username: String!, $password: String!) {
     registeredLogin(username: $username, password: $password) {
