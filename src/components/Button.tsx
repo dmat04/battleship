bdf65ef228d552e63565680054032edc9c7d4bce
matej <matej.djerdji@gmail.com>
2024-01-15 22:56:32 +0100

    Expand on Button styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,26 +1,41 @@
 import styled from 'styled-components';
 import { Theme } from './assets/themes/themeDefault';
+import { assertNever } from '../utils/typeUtils';
 
-const ButtonContainer = styled.button<{ theme: Theme }>`
-  background-color: ${(props) => props.theme.colorBg};
+export type ButtonVariant = 'primary' | 'warning' | 'danger';
+
+const Button = styled.button<{ $variant: ButtonVariant, theme: Theme }>`
+  --colorBg: ${(props) => {
+    switch (props.$variant) {
+      case 'primary': return props.theme.colorPrimary;
+      case 'warning': return props.theme.colorSecondary;
+      case 'danger': return props.theme.colorBg;
+      default: return assertNever(props.$variant);
+    }
+  }};
+  --colorBgHover: ${(props) => {
+    switch (props.$variant) {
+      case 'primary': return props.theme.colorBg;
+      case 'warning': return props.theme.colorPrimary;
+      case 'danger': return props.theme.colorPrimary;
+      default: return assertNever(props.$variant);
+    }
+  }};
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: var(--colorBg);
   border: 2px solid black;
+  transition: ${(props) => `all ${props.theme.durationTransitionDefault}ms ease-out`};
+
   padding: ${(props) => props.theme.paddingMin};
   width: 20rem;
 
+
   &:hover {
-    background-color: ${(props) => props.theme.colorSecondary};
+    background-color: var(--colorBgHover);
   }
 `;
-
-interface Props {
-  label: string;
-  onClick: React.MouseEventHandler<HTMLButtonElement>
-}
-
-const Button = ({ label, onClick }: Props) => (
-  <ButtonContainer onClick={onClick}>
-    {label}
-  </ButtonContainer>
-);
 
 export default Button;
