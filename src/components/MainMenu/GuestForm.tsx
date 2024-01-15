da27896dc715f1e2f048130864a0e99b8d0a9191
matej <matej.djerdji@gmail.com>
2024-01-15 18:50:22 +0100

    Add loading indicators to GuestForm

--- src/components/MainMenu/GuestForm.tsx
+++ src/components/MainMenu/GuestForm.tsx
@@ -3,22 +3,38 @@ import TextInput from './TextInput';
 import { Theme } from '../assets/themes/themeDefault';
 import FormButton from './FormButton';
 import useUsernameChecker from '../../hooks/useUsernameChecker';
+import { useAppDispatch, useAppSelector } from '../../store/store';
+import { guestLogin } from '../../store/authSlice';
+import SpinnerAnim from '../assets/icons/180-ring.svg';
 
 const Container = styled.form<{ theme: Theme }>`
   display: grid;
-  grid-template-rows: 0.75fr 0.5fr 1fr;
+  grid-template-rows: 0.75fr 0.75fr 1fr;
   grid-template-areas: 
-    "username username"
-    "status status"
-    "pick skip";
+    "username"
+    "status"
+    "button";
   grid-row-gap: ${(props) => props.theme.paddingMin};
   grid-column-gap: ${(props) => props.theme.paddingMin};
+  /* align-items: stretch; */
   padding: ${(props) => props.theme.paddingMin} 0 0 0;
 `;
 
-const Status = styled.p`
+const Status = styled.div`
   grid-area: status;
   font-size: smaller;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const Spinner = styled.div<{ $visible: boolean }>`
+  opacity: ${(props) => (props.$visible ? 1 : 0)};
+  background: url(${SpinnerAnim});
+  background-position: center;
+  background-repeat: no-repeat;
+  width: 2rem;
+  aspect-ratio: 1;
 `;
 
 interface Props {
@@ -30,15 +46,29 @@ const GuestForm = ({ disabled }: Props) => {
     username,
     setUsername,
     checkIsPending,
-    errorMessage,
+    message,
+    isValid,
   } = useUsernameChecker(
-    'Pick a username or skip and have one picked for you',
+    'Pick a username or continue and have one picked for you',
     'Good to go!',
     'Username is taken',
   );
 
+  const dispatch = useAppDispatch();
+  const { loginRequestPending } = useAppSelector((state) => state.auth);
+
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = (ev) => {
+    ev.preventDefault();
+
+    if (username.length === 0) {
+      dispatch(guestLogin(null));
+    } else if (isValid) {
+      dispatch(guestLogin(username));
+    }
+  };
+
   return (
-    <Container>
+    <Container onSubmit={handleSubmit}>
       <TextInput
         type="text"
         placeholder="Pick a username?"
@@ -47,20 +77,21 @@ const GuestForm = ({ disabled }: Props) => {
         onChange={(ev) => setUsername(ev.target.value)}
         disabled={disabled}
       />
-      <Status>{errorMessage}</Status>
+      <Status>
+        <p>{message}</p>
+        <Spinner $visible={checkIsPending} />
+      </Status>
       <FormButton
-        $variant="submit"
-        style={{ gridArea: 'pick' }}
-        disabled={disabled}
-      >
-        Pick
-      </FormButton>
-      <FormButton
+        type="submit"
         $variant="skip"
-        style={{ gridArea: 'skip' }}
+        style={{ gridArea: 'button' }}
         disabled={disabled}
       >
-        Skip
+        {
+          loginRequestPending
+            ? <Spinner $visible />
+            : <div>Continue</div>
+        }
       </FormButton>
     </Container>
   );
