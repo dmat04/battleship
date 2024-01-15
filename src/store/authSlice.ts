478df9daed1b0502befff2f90a88bd84c072caad
matej <matej.djerdji@gmail.com>
2024-01-15 17:16:53 +0100

    Removed unnecessary slice state

--- src/store/authSlice.ts
+++ src/store/authSlice.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-param-reassign */
 import { PayloadAction, createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { LoginResult, UsernameQueryResult } from '../__generated__/graphql';
+import { LoginResult } from '../__generated__/graphql';
 import LocalStorage from '../utils/localStorageUtils';
 import { GUEST_LOGIN } from '../graphql/mutations';
 import { CHECK_USERNAME } from '../graphql/queries';
@@ -31,17 +31,11 @@ export const checkUsername = createAsyncThunk(
 export interface AuthSliceState {
   loginResult: LoginResult | null;
   loginRequestPending: boolean;
-  checkUsernameResult: UsernameQueryResult | null;
-  checkUsernamePending: boolean;
-  checkUsernamePendingRequestID: string | null;
 }
 
 const initialState: AuthSliceState = {
   loginResult: LocalStorage.getAccessToken(),
   loginRequestPending: false,
-  checkUsernameResult: null,
-  checkUsernamePending: false,
-  checkUsernamePendingRequestID: null,
 };
 
 const authSlice = createSlice({
@@ -67,28 +61,6 @@ const authSlice = createSlice({
     });
     builder.addCase(guestLogin.rejected, (state) => {
       state.loginRequestPending = false;
-    });
-    builder.addCase(checkUsername.pending, (state, action) => {
-      state.checkUsernamePendingRequestID = action.meta.requestId;
-      state.checkUsernamePending = true;
-    });
-    builder.addCase(checkUsername.fulfilled, (state, action) => {
-      if (action.meta.requestId !== state.checkUsernamePendingRequestID) {
-        return;
-      }
-
-      state.checkUsernamePending = false;
-      state.checkUsernamePendingRequestID = null;
-      state.checkUsernameResult = action.payload?.data.checkUsername ?? null;
-    });
-    builder.addCase(checkUsername.rejected, (state, action) => {
-      if (action.meta.requestId !== state.checkUsernamePendingRequestID) {
-        return;
-      }
-
-      state.checkUsernamePendingRequestID = null;
-      state.checkUsernamePending = false;
-      state.checkUsernameResult = null;
     });
   },
 });
