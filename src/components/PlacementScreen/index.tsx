8f245abbc7b344eb44b20e06f08c27ff3f6a87e8
matej <matej.djerdji@gmail.com>
2024-01-17 15:00:44 +0100

    Implemented PlacementScreen component

--- src/components/PlacementScreen/index.tsx
+++ src/components/PlacementScreen/index.tsx
@@ -1,17 +1,60 @@
-import { Navigate } from "react-router-dom";
-import { useAppDispatch, useAppSelector } from "../../store/store";
+import { Navigate } from 'react-router-dom';
+import styled from 'styled-components';
+import { useAppDispatch, useAppSelector } from '../../store/store';
+import PlacementGrid from './PlacementGrid';
+import { Button } from '../Button';
+import { Theme } from '../assets/themes/themeDefault';
+import { submitPlacement } from '../../store/shipPlacementSlice';
+
+const Container = styled.div<{ theme: Theme }>`
+  display: flex;
+  flex-direction: column;
+  gap: ${(props) => props.theme.paddingSm};
+
+  @media (max-width: 35em) {
+    gap: ${(props) => props.theme.paddingMin};
+  }
+`;
+
+const Header = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Info = styled.p`
+  font-size: larger;
+`;
 
 const PlacementScreen = () => {
   const dispatch = useAppDispatch();
   const roomID = useAppSelector((state) => state.gameRoom.roomID);
-  const gameSettings = useAppSelector((state) => state.gameRoom.gameSettings);
+  const shipPlacementState = useAppSelector((state) => state.shipPlacement);
+  const nonPlacedCount = useAppSelector((state) => state.shipPlacement.nonPlacedIDs.length);
 
-  if (!roomID) return <Navigate to="start" replace />;
+  if (!roomID) return <Navigate to="/start" replace />;
 
-  if (!gameSettings) return null;
+  if (shipPlacementState.shipStates.length === 0) return null;
+
+  const handleSubmit = () => {
+    dispatch(submitPlacement(roomID));
+  };
 
   return (
-
+    <Container>
+      <Header>
+        <Info>Place your ships</Info>
+        <Button
+          $variant="primary"
+          disabled={nonPlacedCount > 0}
+          onClick={handleSubmit}
+        >
+          Start
+        </Button>
+      </Header>
+      <PlacementGrid />
+    </Container>
   );
 };
 
