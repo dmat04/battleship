3a4541fcbf36edde43b513e3c87688b934f07183
matej <matej.djerdji@gmail.com>
2024-01-17 22:06:15 +0100

    Created activeGameSlice, copied messageTypes from server project

---
+++ src/store/activeGameSlice/messageTypes.ts
@@ -0,0 +1,78 @@
+import { GameRoomStatus, ShipPlacement } from '../../__generated__/graphql';
+
+export interface MoveResult {
+  hit: boolean,
+  gameWon: boolean,
+  shipSunk?: ShipPlacement,
+}
+
+export enum ClientMessageCode {
+  Shoot = 'Shoot',
+  RoomStatusRequest = 'RoomStatusRequest',
+}
+
+export enum ServerMessageCode {
+  Error = 'Error',
+  OpponentMoveResult = 'OpponentMoveResult',
+  OwnMoveResult = 'OwnMoveResult',
+  RoomStatusResponse = 'RoomStatusResponse',
+  AuthenticatedResponse = 'AuthenticatedResponse',
+  GameStarted = 'GameStarted',
+}
+
+export interface CoordinateMessage {
+  x: number;
+  y: number;
+}
+
+interface MoveResultMessage extends CoordinateMessage {
+  result: MoveResult;
+  currentPlayer: string;
+}
+
+export interface ShootMessage extends CoordinateMessage {
+  code: ClientMessageCode.Shoot;
+}
+
+export interface RoomStatusRequestMessage {
+  code: ClientMessageCode.RoomStatusRequest;
+}
+
+export interface ErrorMessage {
+  code: ServerMessageCode.Error;
+  message: string;
+}
+
+export interface OpponentMoveResultMessage extends MoveResultMessage {
+  code: ServerMessageCode.OpponentMoveResult;
+}
+
+export interface OwnMoveResultMessage extends MoveResultMessage {
+  code: ServerMessageCode.OwnMoveResult;
+}
+
+export interface RoomStatusResponseMessage {
+  code: ServerMessageCode.RoomStatusResponse;
+  roomStatus: GameRoomStatus;
+}
+
+export interface AuthenticatedResponseMessage {
+  code: ServerMessageCode.AuthenticatedResponse;
+}
+
+export interface GameStartedMessage {
+  code: ServerMessageCode.GameStarted;
+  playsFirst: string;
+}
+
+export type ClientMessage =
+  | ShootMessage
+  | RoomStatusRequestMessage;
+
+export type ServerMessage =
+ | ErrorMessage
+ | OpponentMoveResultMessage
+ | OwnMoveResultMessage
+ | RoomStatusResponseMessage
+ | AuthenticatedResponseMessage
+ | GameStartedMessage;
