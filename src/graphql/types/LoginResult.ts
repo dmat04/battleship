dd710d288720a22aa21bbfae58cc630539cfc2b9
matej <matej.djerdji@gmail.com>
2024-01-18 18:40:17 +0100

    Revert "Change expiration timestamp for access token to be an int instead of string"
    This reverts commit b2250f7b2afe03b04dc06bcef53b4ec5351b6435.

--- src/graphql/types/LoginResult.ts
+++ src/graphql/types/LoginResult.ts
@@ -1,14 +1,14 @@
 export interface LoginResult {
   username: string,
   accessToken: string,
-  expiresAt: number
+  expiresAt: string
 }
 
 export const typeDefs = `#graphql
   type LoginResult {
     username: String!
     accessToken: String!
-    expiresAt: Int!
+    expiresAt: String!
   }
 `;
 
