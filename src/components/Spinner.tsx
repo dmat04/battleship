6073fad1de3dd0aba5e20ad8a5e388e68ab74c98
matej <matej.djerdji@gmail.com>
2024-02-15 22:58:23 +0100

    Spinner - add success state

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,13 +1,19 @@
 import styled from 'styled-components';
 import SpinnerAnim from './assets/icons/180-ring.svg';
+import CheckmarkIcon from './assets/icons/ic_checkmark_cricle.svg';
 
-const Spinner = styled.div<{ $visible: boolean }>`
+const Spinner = styled.div<{ $visible: boolean, $success?: boolean }>`
   opacity: ${(props) => (props.$visible ? 1 : 0)};
-  background: url(${SpinnerAnim});
+  background: ${(props) => (props.$success ? `url(${CheckmarkIcon})` : `url(${SpinnerAnim})`)};
   background-position: center;
   background-repeat: no-repeat;
   width: 2rem;
   aspect-ratio: 1;
 `;
+
+Spinner.defaultProps = {
+  $visible: true,
+  $success: false,
+};
 
 export default Spinner;
