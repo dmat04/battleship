8f401c33c5dc9e5e13d41844f81054a685d1bcd6
matej <matej.djerdji@gmail.com>
2024-02-17 19:41:48 +0100

    Dispatch notifications for some server messages

--- src/store/messageListenerMiddleware/index.ts
+++ src/store/messageListenerMiddleware/index.ts
@@ -2,21 +2,48 @@ import { Middleware } from '@reduxjs/toolkit';
 import { messageReceived } from '../wsMiddleware/actions';
 import { ServerMessageCode } from '../wsMiddleware/messageTypes';
 import { NotificationType } from '../notificationSlice/stateTypes';
-import type { AppDispatch } from '../store';
+import type { AppDispatch, RootState } from '../store';
 import { PushTransientNotification } from '../notificationSlice';
+import { assertNever } from '../../utils/typeUtils';
 
 const messageListenerMiddleware: Middleware = (
-  { dispatch }: { dispatch: AppDispatch },
+  { dispatch, getState }: { dispatch: AppDispatch, getState: () => RootState },
 ) => (next) => (action) => {
   if (messageReceived.match(action)) {
     const message = action.payload;
+    const { code } = message;
+    const state = getState();
 
-    if (message.code === ServerMessageCode.OpponentDisconnected) {
-      dispatch(PushTransientNotification({
-        type: NotificationType.Warning,
-        message: 'The opponent has disconnected',
-        timeoutArg: 5000,
-      }));
+    switch (code) {
+      case ServerMessageCode.Error: break;
+      case ServerMessageCode.OpponentMoveResult: break;
+      case ServerMessageCode.OwnMoveResult: break;
+      case ServerMessageCode.RoomStatusResponse: break;
+      case ServerMessageCode.AuthenticatedResponse: break;
+      case ServerMessageCode.GameStarted: {
+        const { playerName } = state.gameRoom;
+        const { playsFirst } = message;
+        const notification = playsFirst === playerName
+          ? 'Game started - make the first move'
+          : `Game started - ${playsFirst} makes the first move`;
+
+        dispatch(PushTransientNotification({
+          type: NotificationType.Info,
+          message: notification,
+          timeoutArg: 5000,
+        }));
+        break;
+      }
+      case ServerMessageCode.OpponentDisconnected:
+        if (!state.gameRoom.gameStarted) {
+          dispatch(PushTransientNotification({
+            type: NotificationType.Warning,
+            message: 'The opponent has disconnected',
+            timeoutArg: 5000,
+          }));
+        }
+        break;
+      default: assertNever(code);
     }
   }
 
