5ab6896053fb4b751d1e2b66401d448818f9b80b
matej <matej.djerdji@gmail.com>
2024-02-20 16:21:53 +0100

    Render Logo svg inside a container div

--- src/components/Navbar/Logo.tsx
+++ src/components/Navbar/Logo.tsx
@@ -1,6 +1,12 @@
 import styled from 'styled-components';
 import { Theme } from '../assets/themes/themeDefault';
 
+const Container = styled.div<{ theme: Theme }>`
+  width: min(100%, 75rem);
+  margin-inline: auto;
+  padding-block: ${(props) => props.theme.paddingSm};
+`;
+
 const SvgText = styled.text<{ theme: Theme }>`
   font-weight: 900;
   font-stretch: normal;
@@ -19,30 +25,31 @@ const WIDTH = 700;
 const HEIGHT = 100;
 
 const Logo = () => (
-  <svg
-    viewBox={`0 0 ${WIDTH} ${HEIGHT}`}
-    version="1.1"
-    id="svg1"
-    xmlns="http://www.w3.org/2000/svg"
-    style={{
-      paddingBlock: '3rem',
-      mixBlendMode: 'difference',
-    }}
-  >
-    <defs id="defs1" />
-    <g id="layer1">
-      <rect width={WIDTH} height={HEIGHT} fill="none" stroke="none" />
-      <SvgText
-        x={WIDTH / 2}
-        y={HEIGHT / 2}
-        dominantBaseline="middle"
-        textAnchor="middle"
-        id="text1"
-      >
-        Battleship
-      </SvgText>
-    </g>
-  </svg>
+  <Container>
+    <svg
+      viewBox={`0 0 ${WIDTH} ${HEIGHT}`}
+      version="1.1"
+      id="svg1"
+      xmlns="http://www.w3.org/2000/svg"
+      style={{
+        mixBlendMode: 'difference',
+      }}
+    >
+      <defs id="defs1" />
+      <g id="layer1">
+        <rect width={WIDTH} height={HEIGHT} fill="none" stroke="none" />
+        <SvgText
+          x={WIDTH / 2}
+          y={HEIGHT / 2}
+          dominantBaseline="middle"
+          textAnchor="middle"
+          id="text1"
+        >
+          Battleship
+        </SvgText>
+      </g>
+    </svg>
+  </Container>
 );
 
 export default Logo;
