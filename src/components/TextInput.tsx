5db2cf705552f379236999666309cb01334c5945
matej <matej.djerdji@gmail.com>
2024-02-21 00:55:27 +0100

    Style TextInput

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -3,6 +3,8 @@ import { Theme } from './assets/themes/themeDefault';
 
 const TextInput = styled.input<{ theme: Theme }>`
   padding: ${(props) => props.theme.paddingMin};
+  border: 2px solid ${(props) => props.theme.colors.surfacePrimary};
+  border-radius: 9999px;
 
   &:focus {
     outline-offset: -2px;
