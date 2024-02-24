acd1c72358b8523800ff1bec18a09d7f09c14c83
matej <matej.djerdji@gmail.com>
2024-02-25 00:43:06 +0100

    Update stub state data

--- src/store/gameRoomSlice/stateStub.ts
+++ src/store/gameRoomSlice/stateStub.ts
@@ -1,8 +1,88 @@
 import { ShipClassName, ShipOrientation } from '../../__generated__/graphql';
 import { GameResult, PlayerStatus, SliceStateActive } from './stateTypes';
 
+const stubScore = {
+  hitCells: [
+    { x: 7, y: 0 },
+    { x: 8, y: 0 },
+    { x: 2, y: 1 },
+    { x: 4, y: 5 },
+  ],
+  missedCells: [
+    { x: 0, y: 0 },
+    { x: 1, y: 0 },
+    { x: 2, y: 0 },
+    { x: 5, y: 1 },
+    { x: 7, y: 4 },
+    { x: 7, y: 5 },
+    { x: 7, y: 8 },
+    { x: 6, y: 8 },
+  ],
+  sunkenShips: [
+    {
+      ship: {
+        shipID: 'CRUISER-0',
+        size: 3,
+        type: ShipClassName.Cruiser,
+      },
+      orientation: ShipOrientation.Horizontal,
+      x: 0,
+      y: 3,
+    },
+    {
+      ship: {
+        shipID: 'DESTROYER-0',
+        size: 2,
+        type: ShipClassName.Destroyer,
+      },
+      orientation: ShipOrientation.Horizontal,
+      x: 7,
+      y: 2,
+    },
+    {
+      ship: {
+        shipID: 'SUBMARINE-0',
+        size: 1,
+        type: ShipClassName.Submarine,
+      },
+      orientation: ShipOrientation.Horizontal,
+      x: 1,
+      y: 7,
+    },
+  ],
+  inaccessibleCells: [
+    { x: 0, y: 2 },
+    { x: 0, y: 4 },
+    { x: 0, y: 6 },
+    { x: 0, y: 7 },
+    { x: 0, y: 8 },
+    { x: 1, y: 2 },
+    { x: 1, y: 4 },
+    { x: 1, y: 6 },
+    { x: 1, y: 8 },
+    { x: 2, y: 2 },
+    { x: 2, y: 4 },
+    { x: 2, y: 6 },
+    { x: 2, y: 7 },
+    { x: 2, y: 8 },
+    { x: 3, y: 2 },
+    { x: 3, y: 3 },
+    { x: 3, y: 4 },
+    { x: 6, y: 2 },
+    { x: 9, y: 2 },
+    { x: 6, y: 1 },
+    { x: 7, y: 1 },
+    { x: 8, y: 1 },
+    { x: 9, y: 1 },
+    { x: 6, y: 3 },
+    { x: 7, y: 3 },
+    { x: 8, y: 3 },
+    { x: 9, y: 3 },
+  ],
+};
+
 const stateStub: SliceStateActive = {
-  roomID: '',
+  roomID: 'asdf',
   inviteCode: '',
   requestStatus: {
     loadingJoinRoom: false,
@@ -11,11 +91,11 @@ const stateStub: SliceStateActive = {
   },
   playerName: 'PlayerName',
   opponentName: 'OpponentName',
-  playerStatus: PlayerStatus.Connected,
-  opponentStatus: PlayerStatus.Disconnected,
+  playerStatus: PlayerStatus.Ready,
+  opponentStatus: PlayerStatus.Ready,
   gameStarted: true,
   round: 10,
-  gameResult: GameResult.OpponentWon,
+  gameResult: null,
   gameSettings: {
     boardHeight: 10,
     boardWidth: 10,
@@ -58,7 +138,7 @@ const stateStub: SliceStateActive = {
       },
     ],
   },
-  currentPlayer: 'PlayerName',
+  currentPlayer: 'OpponentName',
   playerShips: [
     {
       ship: {
@@ -131,69 +211,8 @@ const stateStub: SliceStateActive = {
       y: 7,
     },
   ],
-  playerScore: {
-    hitCells: [
-      { x: 0, y: 1 },
-      { x: 1, y: 7 },
-      { x: 9, y: 6 },
-      { x: 5, y: 1 },
-    ],
-    missedCells: [
-      { x: 2, y: 0 },
-      { x: 9, y: 2 },
-      { x: 5, y: 7 },
-      { x: 0, y: 9 },
-    ],
-    sunkenShips: [
-      {
-        ship: {
-          shipID: 'CARRIER-0',
-          size: 5,
-          type: ShipClassName.Carrier,
-        },
-        orientation: ShipOrientation.Horizontal,
-        x: 0,
-        y: 1,
-      },
-      {
-        ship: {
-          shipID: 'CRUISER-0',
-          size: 3,
-          type: ShipClassName.Cruiser,
-        },
-        orientation: ShipOrientation.Horizontal,
-        x: 0,
-        y: 3,
-      },
-      {
-        ship: {
-          shipID: 'DESTROYER-0',
-          size: 2,
-          type: ShipClassName.Destroyer,
-        },
-        orientation: ShipOrientation.Horizontal,
-        x: 7,
-        y: 2,
-      },
-      {
-        ship: {
-          shipID: 'SUBMARINE-0',
-          size: 1,
-          type: ShipClassName.Submarine,
-        },
-        orientation: ShipOrientation.Horizontal,
-        x: 1,
-        y: 7,
-      },
-    ],
-    inaccessibleCells: [],
-  },
-  opponentScore: {
-    hitCells: [],
-    missedCells: [],
-    sunkenShips: [],
-    inaccessibleCells: [],
-  },
+  playerScore: stubScore,
+  opponentScore: stubScore,
 };
 
 export default stateStub;
