be11a1d3e1222baf0fc135a1e4ed48d37c39fa46
matej <matej.djerdji@gmail.com>
2024-07-18 21:35:41 +0200

    Flat eslint config with ts support

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint';
+import pluginJest from 'eslint-plugin-jest';
+import pluginReact from 'eslint-plugin-react';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parserOptions: {
+        project: [
+          "./client/tsconfig.json",
+          "./server/tsconfig.json",
+          "./common/tsconfig.json",
+        ],
+        tsconfigRootDir: import.meta.dirname,
+      }
+    },
+    plugins: {
+      pluginJest,
+      pluginReact,
+    },
+  },
+  {
+    ignores: [
+      "**/*.config.js"
+    ]
+  }
+);
