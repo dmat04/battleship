94cec4a40e11a4bd4dd20e1ff4344865c66211df
matej <matej.djerdji@gmail.com>
2024-07-20 15:13:18 +0200

    Move ApolloContext definition into common workspace

---
+++ common/utils/ApolloContext.ts
@@ -0,0 +1,33 @@
+import { GraphQLError } from "graphql";
+import type { ExpressContextFunctionArgument } from "@apollo/server/express4";
+
+export interface ApolloContext {
+  authToken: string | null;
+}
+
+export const assertAuthorized = (context: ApolloContext): string => {
+  const token = context.authToken;
+
+  if (!token) {
+    throw new GraphQLError("Acess token missing in request header", {
+      extensions: { code: "UNAUTHENTICATED" },
+    });
+  }
+
+  return token;
+};
+
+export const contextFn = async (
+  args: ExpressContextFunctionArgument,
+): Promise<ApolloContext> => {
+  const { authorization } = args.req.headers;
+  if (authorization && authorization.startsWith("Bearer ")) {
+    return {
+      authToken: authorization.replace("Bearer ", ""),
+    };
+  }
+
+  return {
+    authToken: null,
+  };
+};
