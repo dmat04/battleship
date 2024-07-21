4b3953ff7e62b93d654364b5404de17f7553acd7
matej <matej.djerdji@gmail.com>
2024-07-21 20:45:48 +0200

    graphql-codegen configuration changes
    Generate base, resolver and operation types using the respective plugins. After those are generated, generate operation hook types for the client, and lastly resolver files using the @eddeee888/gcg-typescript-resolver-files plugin, but deleting its generated base types, and fix imports in the generated resolver files using a codegen hook (beforeDone hook).

---
+++ codegen/config.ts
@@ -0,0 +1,84 @@
+import { CodegenConfig } from "@graphql-codegen/cli";
+import { defineConfig } from "@eddeee888/gcg-typescript-resolver-files";
+
+const config: CodegenConfig = {
+  schema: "./common/graphql-schema/**/schema.graphql",
+  documents: "./client/src/graphql/*.ts",
+  hooks: {
+    afterOneFileWrite: ["prettier --write"],
+    beforeDone: [
+      "del ./server/src/graphql/__generated__/resolverTypes.generated.ts",
+      "node --loader ts-node/esm ./codegen/fixImports.ts"
+    ]
+  },
+  generates: {
+    "./common/types/__generated__/types.generated.ts": {
+      plugins: [
+        {
+          add: {
+            content: [
+              `import type { ApolloContext } from "@battleship/common/utils/ApolloContext";`
+            ]
+          },
+        },
+        {
+          "typescript": {
+            enumsAsTypes: false,
+            namingConvention: "change-case-all#pascalCase",
+          }
+        },
+        {
+          "typescript-resolvers": {
+            namingConvention: "change-case-all#pascalCase",
+            contextType: "ApolloContext",
+          }
+        },
+        {
+          "typescript-operations": {
+            namingConvention: "change-case-all#pascalCase",
+          },
+        },
+      ],
+    },
+    "./client/src/graphql/__generated__/operationHooks.ts": {
+      plugins: [
+        {
+          add: {
+            content: [
+              `import * as Types from "@battleship/common/types/__generated__/types.generated";`,
+            ]
+          },
+        },
+        {
+          "typescript-react-apollo": {
+            importOperationTypesFrom: "Types",
+          },
+        },
+      ],
+    },
+    "./server/src/graphql/__generated__": defineConfig({
+      mode: "merged",
+      resolverTypesPath: "./resolverTypes.generated.ts",
+      resolverRelativeTargetDir: "../resolvers",
+      resolverMainFileMode: "merged",
+      add: {
+        './resolverTypes.generated.ts': {
+          content: [
+            `import * as Types from "@battleship/common/types/__generated__/types.generated";`,
+            `import type { ApolloContext } from "@battleship/common/utils/ApolloContext";`
+          ],
+        }
+      },
+      typesPluginsConfig: {
+        enumsAsTypes: false,
+        namespacedImportName: "Types",
+        contextType: "ApolloContext",
+        namingConvention: "change-case-all#pascalCase",
+      },
+      mergeSchema: false,
+    }),
+
+  },
+};
+
+export default config;
---
+++ codegen/fixImports.ts
@@ -0,0 +1,9 @@
+import { replaceInFileSync } from 'replace-in-file';
+
+const options = {
+  files: './server/src/graphql/**/**/*.ts',
+  from: /\.\/(\.\.\/)*(__generated__\/)*resolverTypes\.generated/,
+  to: '@battleship/common/types/__generated__/types.generated'
+}
+
+replaceInFileSync(options)
