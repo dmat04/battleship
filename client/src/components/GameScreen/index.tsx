7e36e9abd6df0db82dfab13445d5b840ad29c990
matej <matej.djerdji@gmail.com>
2024-07-26 17:23:54 +0200

    Fixed imports

--- client/src/components/GameScreen/index.tsx
+++ client/src/components/GameScreen/index.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
-import styled from "styled-components";
-import StatusHeader from "./StatusHeader";
-import { Theme } from "../assets/themes/themeDefault";
+import { styled } from "styled-components";
+import StatusHeader from "./StatusHeader/index.js";
+import { Theme } from "../assets/themes/themeDefault.js";
 
 const Container = styled.div<{ theme: Theme }>`
   display: flex;
