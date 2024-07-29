a9671f99f841b4810e906e24f371205504a05db6
matej <matej.djerdji@gmail.com>
2024-07-29 19:35:08 +0200

    Client: configure vitest for react component testing

---
+++ client/vitest.config.js
@@ -0,0 +1,20 @@
+import { mergeConfig, defineProject } from "vitest/config";
+import rootConfig from "../vitest.config";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+
+export default mergeConfig(
+  rootConfig,
+  defineProject({
+    plugins: [
+      react(),
+      svgr({
+        include: "**/*.svg"
+      })],
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: "./test/setup.js",
+    },
+  }),
+);
--- eslint.config.js
+++ eslint.config.js
@@ -25,6 +25,6 @@ export default tseslint.config(
     },
   },
   {
-    ignores: ["**/*.config.js", "**/*.config.ts", "**/build/"],
+    ignores: ["**/*.config.js", "**/*.config.ts", "**/build/", "**/test/setup.js"],
   },
 );
