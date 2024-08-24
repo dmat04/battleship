0918a7a99abe6c4115840c0ba3338486d0c85243
matej <matej.djerdji@gmail.com>
2024-08-24 17:30:47 +0200

    Created mock state data

---
+++ packages/client/test/reduxStateData/authSliceTestdata.ts
@@ -0,0 +1,15 @@
+import { AuthSliceState } from "../../src/store/authSlice.js";
+
+export const noUser: AuthSliceState = {
+  loginRequestPending: false,
+  loginResult: null,
+};
+
+export const userAuthenticated: AuthSliceState = {
+  loginRequestPending: false,
+  loginResult: {
+    accessToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Ikd1ZXN0IzIzNzAzIiwiaWF0IjoxNzI0NTEwMzQxLCJleHAiOjE3MjQ1OTY3NDF9.-uhSJqsNGmHLSMSWiGje8f-8o8pxZQF9PwV_B8_IVpw',
+    expiresAt: `${Date.now() + 24 * 3600 * 1000}`,
+    username: 'Guest#23703',
+  },
+};
