d5ac3280afd3f57f9718ecdb4f01b41dda6d718f
matej <matej.djerdji@gmail.com>
2024-08-26 18:36:55 +0200

    Create an apollo client instance for each test

--- packages/client/test/setup.ts
+++ packages/client/test/setup.ts
@@ -1,9 +1,16 @@
 import "@testing-library/jest-dom/vitest";
-import { expect, afterEach } from "vitest";
+import { expect, afterEach, beforeEach } from "vitest";
 import { cleanup } from "@testing-library/react";
 import * as matchers from "@testing-library/jest-dom/matchers";
+import createApolloClient from "../src/utils/apolloClient.js";
+import Dependencies from "../src/utils/Dependencies.js";
 
 expect.extend(matchers);
+
+beforeEach(() => {
+  const apolloClient = createApolloClient();
+  Dependencies.setApolloClient(apolloClient);
+});
 
 afterEach(() => {
   cleanup();
