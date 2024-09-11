fc996cbe54a6fe82826fd17cbbc2cb45d3b8457f
matej <matej.djerdji@gmail.com>
2024-09-11 16:40:56 +0200

    Created the Session db model

---
+++ packages/common/src/dbModels/SessionDbModel.ts
@@ -0,0 +1,23 @@
+import { Model, Schema, model } from "mongoose";
+import { User, userIdExists } from "@battleship/common/dbModels/UserDbModels.js";
+
+export interface Session {
+  readonly id: string;
+  readonly user: User;
+}
+
+const sessionSchema = new Schema<Session, Model<Session>>({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+    validate: {
+      validator: (id: Schema.Types.ObjectId) => userIdExists(id),
+      message: "No User with provided id exists",
+    }
+  },
+});
+
+const SessionDbModel = model<Session, Model<Session>>("Session", sessionSchema);
+
+export default SessionDbModel;
