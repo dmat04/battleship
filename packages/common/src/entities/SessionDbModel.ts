9917f4ffa6ec1c78e1993632115870159e3821b9
matej <matej.djerdji@gmail.com>
2024-09-14 00:14:38 +0200

    Added toObject options to Session mongoose schema.

--- packages/common/src/entities/SessionDbModel.ts
+++ packages/common/src/entities/SessionDbModel.ts
@@ -6,17 +6,32 @@ export interface Session {
   readonly user: User;
 }
 
-const sessionSchema = new Schema<Session, Model<Session>>({
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-    validate: {
-      validator: (id: Types.ObjectId) => userExists(id),
-      message: "No User with provided id exists",
-    }
+const toObjectOptions = {
+  versionKey: false,
+  flattenObjectIds: true,
+  transform: (_: unknown, ret: Record<string, unknown>) => {
+    ret.id = ret._id;
+    delete ret._id;
+    return ret;
   },
-});
+};
+
+const sessionSchema = new Schema<Session, Model<Session>>(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      validate: {
+        validator: (id: Types.ObjectId) => userExists(id),
+        message: "No User with provided id exists",
+      },
+    },
+  },
+  {
+    toObject: toObjectOptions,
+  },
+);
 
 const SessionDbModel = model<Session, Model<Session>>("Session", sessionSchema);
 
