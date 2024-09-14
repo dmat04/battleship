27896a458964aaed23fed1a7c77349761de1ba8d
matej <matej.djerdji@gmail.com>
2024-09-14 18:01:40 +0200

    Add more constructor options for ValidationError

--- packages/common/src/repositories/Errors.ts
+++ packages/common/src/repositories/Errors.ts
@@ -4,7 +4,7 @@ export class RepositoryError extends Error {
   constructor(original: unknown) {
     super("An unknown Repository error has occured", { cause: original });
   }
-};
+}
 
 export class EntityNotFoundError extends Error {
   constructor(
@@ -15,31 +15,47 @@ export class EntityNotFoundError extends Error {
       `Entity of type ${entityType} with identifier '${entityIdentifier}' wasn't found.`,
     );
   }
-};
+}
+
+interface PropertyError {
+  value: string;
+  message: string;
+}
+
+type ValidationErrorParams =
+  | MongooseError.ValidationError
+  | { propertyName: string; error: PropertyError }[];
 
 export class ValidationError extends Error {
   invalidProperties: {
-    [property: string]: {
-      value: string;
-      message: string;
-    };
+    [property: string]: PropertyError;
   } = {};
 
-  constructor(mongooseError: MongooseError.ValidationError) {
-    super(mongooseError.message);
+  constructor(args: ValidationErrorParams) {
+    if (args instanceof MongooseError.ValidationError) {
+      super(args.message);
 
-    Object.entries(mongooseError.errors).forEach(([prop, err]) => {
-      if (err instanceof MongooseError.ValidatorError) {
-        this.invalidProperties[prop] = {
-          value: String(err.value),
-          message: err.message,
-        };
-      } else {
-        this.invalidProperties[prop] = {
-          value: err.stringValue,
-          message: "Error casting value",
-        };
-      }
-    });
+      Object.entries(args.errors).forEach(([prop, err]) => {
+        if (err instanceof MongooseError.ValidatorError) {
+          this.invalidProperties[prop] = {
+            value: String(err.value),
+            message: err.message,
+          };
+        } else {
+          this.invalidProperties[prop] = {
+            value: err.stringValue,
+            message: "Error casting value",
+          };
+        }
+      });
+    } else {
+      super(
+        "Validation error on properties: " +
+          args.map((p) => p.propertyName).toString(),
+      );
+      args.forEach((err) => {
+        this.invalidProperties[err.propertyName] = err.error;
+      });
+    }
   }
-};
+}
