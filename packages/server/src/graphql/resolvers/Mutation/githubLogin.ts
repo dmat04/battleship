46d8006487a54ca573e33842b92ff97be25b48f7
matej <matej.djerdji@gmail.com>
2024-09-17 18:04:25 +0200

    Implemented the githuLogin gql mutation resolver

--- packages/server/src/graphql/resolvers/Mutation/githubLogin.ts
+++ packages/server/src/graphql/resolvers/Mutation/githubLogin.ts
@@ -1,6 +1,28 @@
 import type { MutationResolvers } from "@battleship/common/types/__generated__/types.generated.js";
+import GithubService from "../../../services/GithubService.js";
+import AuthenticationError from "../../../services/errors/AuthenticationError.js";
+import UserService from "../../../services/UserService.js";
+import SessionService from "../../../services/SessionService.js";
+
 export const githubLogin: NonNullable<
   MutationResolvers["githubLogin"]
-> = async (_parent, _arg, _ctx) => {
-  /* Implement Mutation.githubLogin resolver logic here */
+> = async (_, _arg) => {
+  const accessToken = await GithubService.requestAccessToken(_arg.accessCode);
+  if (!accessToken) {
+    throw new AuthenticationError("token invalid");
+  }
+
+  const userResponse = await GithubService.requestUser(
+    accessToken?.access_token,
+  );
+  if (!userResponse) {
+    throw new AuthenticationError("token invalid");
+  }
+
+  const githubUser = await UserService.createGithubUser(
+    userResponse,
+    accessToken.refresh_token,
+  );
+
+  return SessionService.loginUser(githubUser);
 };
